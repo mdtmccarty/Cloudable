379787cda49a83ab1325d9e7c2e6818cbff2d4fd
Christian Nygaard <neils.nygaard@gmail.com>
2019-02-27 13:41:01 -0700

    Added layout 'xml' files, and an unfinished test

--- app/src/main/java/com/example/cloudable/MainActivity.java
+++ app/src/main/java/com/example/cloudable/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.cloudable;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //this is a comment that I am adding
+    }
+    public void intentLogin(View view) {
+        Intent intent = new Intent(this, MainPageActivity.class);
+        startActivity(intent);
+        System.out.println("creating intent!");
     }
 }
---
+++ app/src/test/java/com/example/cloudable/AdminTest.java
@@ -0,0 +1,4 @@
+package com.example.cloudable;
+
+public class AdminTest {
+}
