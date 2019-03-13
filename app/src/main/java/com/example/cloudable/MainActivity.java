e2db5c411a0efb23590c8738b890d5b50f1d0757
Dihelvid <mdtmccarty@gmail.com>
2019-03-13 13:07:10 -0600

    Made the Login page able to save login information.

--- app/src/main/java/com/example/cloudable/MainActivity.java
+++ app/src/main/java/com/example/cloudable/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cloudable;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         isAdmin = false;
+        SharedPreferences login = getSharedPreferences("loginInfo", MODE_PRIVATE);
+        EditText userName = findViewById(R.id.editText);
+        EditText key = findViewById(R.id.editText2);
+        userName.setText(login.getString("userName", ""));
+        key.setText(login.getString("key", ""));
         //this is a comment that I am adding
     }
     public void intentLogin(View view) {
         isAdmin = true;
+        EditText userName = findViewById(R.id.editText);
+        EditText key = findViewById(R.id.editText2);
+        SharedPreferences sharedPreferences = getSharedPreferences("loginInfo", MODE_PRIVATE);
+        sharedPreferences.edit().putString("userName", userName.getText().toString()).apply();
+        sharedPreferences.edit().putString("key", key.getText().toString()).apply();
         Intent intent = new Intent(this, MainPageActivity.class);
         startActivity(intent);
         System.out.println("creating intent!");
