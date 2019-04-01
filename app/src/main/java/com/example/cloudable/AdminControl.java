faa3546cf494556eb2d19989dbdfbea6fe2494b1
Christian Nygaard <neils.nygaard@gmail.com>
2019-04-01 14:13:00 -0600

    added functionality to create folder button in admin Control, creates new folder in main directory

--- app/src/main/java/com/example/cloudable/AdminControl.java
+++ app/src/main/java/com/example/cloudable/AdminControl.java
@@ -1,20 +1,36 @@
 package com.example.cloudable;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.IOException;
 
 public class AdminControl extends AppCompatActivity {
 
     Button createFolderButton;
+
+    private String m_Text = "";
+    private StorageReference mainFolder;
+    private StorageReference newFolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +47,45 @@ public class AdminControl extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+    }
+
+    public void createFolder(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Folder Name");
+
+        final EditText input = new EditText(this);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+
+                final Gson gson = new Gson();
+                final File localFile;
+                try {
+                    localFile = File.createTempFile("data","json");
+                    mainFolder = FirebaseStorage.getInstance().getReference();
+                    System.out.println("M_TEXT: " + m_Text);
+                    mainFolder.child(m_Text + "/StorageData.json").putFile(Uri.fromFile(localFile));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
 
     }
 }
