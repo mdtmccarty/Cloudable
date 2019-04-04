e9a159344a866eebef38e5a396d7547933deb64f
unknown <dylanwarner93@gmail.com>
2019-04-03 20:28:04 -0600

    more work on the audio player

--- app/src/main/java/com/example/cloudable/AudioPlayer.java
+++ app/src/main/java/com/example/cloudable/AudioPlayer.java
@@ -18,7 +18,10 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -26,6 +29,7 @@ import static java.security.AccessController.getContext;
 
 public class AudioPlayer extends AppCompatActivity {
     private StorageReference mStorageRef;
+    final String FILE_NAME = "audioFiles.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +37,44 @@ public class AudioPlayer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_player);
         mStorageRef = FirebaseStorage.getInstance().getReference();
-        //StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+    }
+
+    public void download(View view){
+        StorageReference anotherStorRef = mStorageRef.child("recording.mp3");
+        final long MAX_SIZE = 1024 * 1024;
+
+        anotherStorRef.getBytes(MAX_SIZE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                FileOutputStream outputStream = null;
+                try {
+                    outputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
+                    outputStream.write(bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (outputStream != null){
+                        try {
+                            outputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    public void play(View view) throws IOException {
+        FileInputStream fis = null;
+        fis = openFileInput(FILE_NAME);
+        InputStreamReader isr = new InputStreamReader(fis);
+        MediaPlayer mp = new MediaPlayer();
+        mp.reset();
+        mp.setDataSource(fis.getFD());
+        mp.prepare();
+        mp.start();
+
     }
 
 //    public void play(View view){
