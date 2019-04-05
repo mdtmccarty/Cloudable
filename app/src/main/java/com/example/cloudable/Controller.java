fd7e79c02177e2192b1a140fd1b5fc76e6419faf
Christian Nygaard <neils.nygaard@gmail.com>
2019-04-05 13:42:59 -0600

    I'm not sure what I did, but it says I have to commit some changes...

--- app/src/main/java/com/example/cloudable/Controller.java
+++ app/src/main/java/com/example/cloudable/Controller.java
@@ -21,6 +21,11 @@ public class Controller {
     Gson gson = new Gson();
     Type token = new TypeToken<ArrayList<FileRecord>>(){}.getType();
     ArrayList<ArrayList<FileRecord>> files;
+
+    public ArrayList<ParsedDirectory> getDirectories() {
+        return directories;
+    }
+
     ArrayList<ParsedDirectory> directories;
     File localFile;
     int index = 0;
